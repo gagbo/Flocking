b7fadcdfcd766332ee42d46ed6157329ed44859c
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-22 18:25:08 +0100

    Doocumentation

--- src/engine/engineantscene.h
+++ src/engine/engineantscene.h
@@ -34,23 +34,30 @@ class EngineAntScene : public QGraphicsScene
     Q_OBJECT
 
   public:
+    //! Default Constructor
     EngineAntScene(float dt = DEFAULT_TIMESTEP, int dim = 2);
+
+    //! Destructor
     ~EngineAntScene();
 
+    //! Constructs a new ant, and add it to this scene
     void add_ant(Victor position, Victor velocity = Victor(0, 0));
 
+    //! Get the current time step
     inline float
     dt()
     {
         return time_step;
     }
 
+    //! Get the list of ants owned by this scene
     inline std::vector<Ant *> &
     list()
     {
         return ant_list;
     }
 
+    //! Set the current time step
     inline void
     set_dt(float dt)
     {
@@ -58,19 +65,37 @@ class EngineAntScene : public QGraphicsScene
     }
 
   signals:
+    //! Emit a message to be read and/or printed by MainWindow
     void message(QString);
+
+    //! Emit a signal to multiply the scale of the view by given factor
     void scale_change(float);
+
+    //! Emit a signal to reset the scale of the view to 1:1
     void scale_reset(void);
 
   protected:
-    std::vector<Ant *> ant_list;
-    int dimension;
-    float time_step;
-    QColor border_color;
+    std::vector<Ant *> ant_list; //!< List of the ants owned by the scene
+    int dimension;               //!< Spatial dimension (probably always 2)
+    float time_step;             //!< Time step of the physics engine
+    QColor border_color; //!< Color of the border to draw where the scene wraps
+                         //! around
 
+    //! Draw a border around the views[0] scene rectangle
+    /*! This is an overloaded method, the second parameter is not used.
+     *  We use views[0] scene rectangle because this is how we choose the
+     *  size of the world to begin with, in the constructor of MainWindow
+     */
     void drawForeground(QPainter *, const QRectF &);
 
+    //! Handle mouse button events to emit signals upwards
+    /*! The Left button emits a message to tell where we clicked
+     *  The middle button resets the scaling of the representation of the scene
+     *  The right button does nothing yet (it should pop a menu someday)
+     */
     void mousePressEvent(QGraphicsSceneMouseEvent *);
+
+    //! Handle mouse scroll events to emit zoom signals to the containing view
     void wheelEvent(QGraphicsSceneWheelEvent *);
 };
 
