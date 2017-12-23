e331edf58af55ca2d7d46cb1883d8798c53a0618
Gerry Agbobada <gagbobada+git@gmail.com>
2017-12-23 12:58:41 +0100

    Add multiple cases for each unit test in Victor [ci skip]
    TOFIX : QtestData is not happy with the QTest::addColumn<float>
    apparently. This is why there is a [ci skip] in the commit message

--- src/geometry/test_geometry.cpp
+++ src/geometry/test_geometry.cpp
@@ -18,35 +18,45 @@
  * SOFTWARE.
  */
 
-#include <QTest>
 #include "victor.h"
+#include <QTest>
 #include <cmath>
 
-class TestGeometry: public QObject
+Q_DECLARE_METATYPE(Victor);
+
+class TestGeometry : public QObject
 {
     Q_OBJECT
-private slots:
+  private slots:
     void access(void);
     void comparison(void);
-    
+    void comparison_data(void);
+
     void pure_assignment(void);
     void addition(void);
+    void addition_data(void);
     void substraction(void);
+    void substraction_data(void);
     void multiplication(void);
+    void multiplication_data(void);
     void division(void);
-    
+    void division_data(void);
+
     void is_2d(void);
     void p_norm(void);
+    void p_norm_data(void);
     void p_normalize(void);
+    void p_normalize_data(void);
     void zero(void);
 };
 
-void TestGeometry::access(void)
+void
+TestGeometry::access(void)
 {
     // Data
     float coord1(1.25);
     float coord2(-3.1);
-    
+
     // Test
     Victor first(coord1, coord2);
     Victor second = Victor(coord1, coord2);
@@ -56,165 +66,271 @@ void TestGeometry::access(void)
     QCOMPARE(second[1], coord2);
 }
 
-void TestGeometry::comparison()
+void
+TestGeometry::comparison_data()
 {
-    // Data
-    Victor first(1.0, 2.3);
-    Victor second(3.1234 / 3.1234, 2.300);
-    bool expected = true;
-    
-    // Test
-    QCOMPARE(first == second, expected);
-    
+    QTest::addColumn<Victor>("first");
+    QTest::addColumn<Victor>("second");
+    QTest::addColumn<bool>("expected");
+
+    QTest::newRow("Same initialisation")
+        << Victor(1, 2) << Victor(1, 2) << true;
+    QTest::newRow("Equal but different init")
+        << Victor(-1 / 24., 3.245) << Victor(-2 / 48., 32450 / 10000.) << true;
+    QTest::newRow("Same dimension, different vectors")
+        << Victor(1.2, -0.3) << Victor(-1.2, 0.3) << false;
+    QTest::newRow("Bad dimension so false first(2d)")
+        << Victor(0.5, 0.8) << Victor(0.5, 0.8, 0) << false;
+    QTest::newRow("Bad dimension so false first(3d)")
+        << Victor(-1.2, 0.342, 0) << Victor(-1.2, 0.342) << false;
 }
 
-void TestGeometry::pure_assignment()
+void
+TestGeometry::comparison()
+{
+    // Data
+    QFETCH(Victor, first);
+    QFETCH(Victor, second);
+    QFETCH(bool, expected);
+
+    // Test
+    QCOMPARE(first == second, expected);
+}
+
+void
+TestGeometry::pure_assignment()
 {
     // Data
     Victor first(0.0, 0.0);
     float coord1 = -1.9;
     float coord2 = 24.2;
     const Victor second(coord1, coord2);
-    
+
     // Test
     first = second;
     QCOMPARE(first[0], second[0]);
     QCOMPARE(first[1], second[1]);
 }
 
+void
+TestGeometry::addition_data(void)
+{
+    QTest::addColumn<Victor>("first");
+    QTest::addColumn<Victor>("second");
+    QTest::addColumn<Victor>("result");
 
-void TestGeometry::addition(void)
+    QTest::newRow("Simple positive")
+        << Victor(1, 2) << Victor(3, 4) << Victor(4, 6);
+    QTest::newRow("Simple negative")
+        << Victor(-1 / 24., -3.245) << Victor(1 / 12., -2)
+        << Victor(1 / 24., -5.245);
+    QTest::newRow("Bad dimension leave result = first(2d)")
+        << Victor(0.5, 0.8) << Victor(1, 1, 4.0) << Victor(0.5, 0.8);
+    QTest::newRow("Bad dimension leave result = first(3d)")
+        << Victor(-1.2, 0.342, 135) << Victor(3, 4) << Victor(-1.2, 0.342, 135);
+}
+
+void
+TestGeometry::addition(void)
 {
     // Data
-    Victor first(1., 2.);
-    Victor second(3., 4.);
-    Victor result(4., 6.);
-    
+    QFETCH(Victor, first);
+    QFETCH(Victor, second);
+    QFETCH(Victor, result);
+
     // Classic operator
     Victor test_victor = first + second;
     QCOMPARE(test_victor[0], result[0]);
     QCOMPARE(test_victor[1], result[1]);
-    
+
     // Assignment operation
     first += second;
     QCOMPARE(first[0], result[0]);
     QCOMPARE(first[1], result[1]);
 }
 
-void TestGeometry::substraction(void)
+void
+TestGeometry::substraction_data(void)
+{
+    QTest::addColumn<Victor>("first");
+    QTest::addColumn<Victor>("second");
+    QTest::addColumn<Victor>("result");
+
+    QTest::newRow("Simple positive")
+        << Victor(1, 2) << Victor(3, 4) << Victor(-2, -2);
+    QTest::newRow("Simple negative")
+        << Victor(-1 / 24., -3.245) << Victor(1 / 12., -4)
+        << Victor(-3 / 24., 0.755);
+    QTest::newRow("Bad dimension leave result = first(2d)")
+        << Victor(0.5, 0.8) << Victor(1, 1, 4.0) << Victor(0.5, 0.8);
+    QTest::newRow("Bad dimension leave result = first(3d)")
+        << Victor(-1.2, 0.342, 135) << Victor(3, 4) << Victor(-1.2, 0.342, 135);
+}
+
+void
+TestGeometry::substraction(void)
 {
     // Data
-    Victor first(1., 6.);
-    Victor second(3., 4.);
-    Victor result(-2., 2.);
-    
+    QFETCH(Victor, first);
+    QFETCH(Victor, second);
+    QFETCH(Victor, result);
+
     // Classic operator
     Victor test_victor = first - second;
     QCOMPARE(test_victor[0], result[0]);
     QCOMPARE(test_victor[1], result[1]);
-    
+
     // Assignment operation
     first -= second;
     QCOMPARE(first[0], result[0]);
     QCOMPARE(first[1], result[1]);
 }
 
-void TestGeometry::multiplication(void)
+void
+TestGeometry::multiplication_data(void)
+{
+    QTest::addColumn<float>("coef");
+    QTest::addColumn<Victor>("second");
+    QTest::addColumn<Victor>("result");
+
+    QTest::newRow("Simple positive") << 1.2 << Victor(3, 4) << Victor(3.6, 4.8);
+    QTest::newRow("Fraction and negative")
+        << -1 / 7. << Victor(1 / 12., -49.) << Victor(-1 / 84., 7);
+    QTest::newRow("Try 0") << 0 << Victor(1, 1) << Victor(0, 0);
+}
+
+void
+TestGeometry::multiplication(void)
 {
     // Data
-    float coef(1.2);
-    Victor second(3., 4.);
-    Victor result(3.6, 4.8);
-    
+    QFETCH(float, coef);
+    QFETCH(Victor, second);
+    QFETCH(Victor, result);
+
     // Classic operator
     Victor test_victor = coef * second;
     QCOMPARE(test_victor[0], result[0]);
     QCOMPARE(test_victor[1], result[1]);
-    
+
     // Assignment operation
     second *= coef;
     QCOMPARE(second[0], result[0]);
     QCOMPARE(second[1], result[1]);
 }
 
-void TestGeometry::division(void)
+void
+TestGeometry::division_data(void)
+{
+    QTest::addColumn<float>("coef");
+    QTest::addColumn<Victor>("second");
+    QTest::addColumn<Victor>("result");
+
+    QTest::newRow("Simple positive") << 1.2 << Victor(3.6, 4.8) << Victor(3, 4);
+    QTest::newRow("Fraction and negative")
+        << -1 / 7. << Victor(-1 / 84., 7.) << Victor(1 / 12., -49.);
+}
+
+void
+TestGeometry::division(void)
 {
     // Data
-    float coef(0.75);
-    Victor second(3., 4.);
-    Victor result(4, 16./3.);
-    
+    QFETCH(float, coef);
+    QFETCH(Victor, second);
+    QFETCH(Victor, result);
+
     // Assignment operation
     second /= coef;
     QCOMPARE(second[0], result[0]);
     QCOMPARE(second[1], result[1]);
 }
 
-void TestGeometry::is_2d()
+void
+TestGeometry::is_2d()
 {
     // Data
     Victor victor_3d(1., 2., 3.);
     Victor victor_2d(1., 2.);
-    
+
     // Test
     QCOMPARE(victor_2d.is_2d(), true);
     QCOMPARE(victor_3d.is_2d(), false);
 }
 
-void TestGeometry::p_norm()
+void
+TestGeometry::p_norm_data(void)
 {
-    // Data
-    float coord1 = 2.3;
-    float coord2 = -1.0;
-    Victor test_victor(coord1, coord2);
-    float norm_2 = std::pow(std::pow(coord1, 2) + std::pow(coord2, 2), 0.5);
-    float norm_3 = std::pow(std::pow(std::abs(coord1), 3) + std::pow(std::abs(coord2), 3), 1/3.);
-    
-    // Test
-    QCOMPARE(test_victor.p_norm(), norm_2);
-    QCOMPARE(test_victor.p_norm(3), norm_3);
-    
+    QTest::addColumn<Victor>("test_victor");
+    QTest::addColumn<int>("p");
+    QTest::addColumn<float>("result");
+
+    QTest::newRow("2-norm of non null vector")
+        << Victor(2.3, -1.0) << 2
+        << std::pow(std::pow(2.3, 2) + std::pow(-1.0, 2), 0.5);
+    QTest::newRow("3-norm of non null vector")
+        << Victor(2.3, -1.0) << 3
+        << std::pow(std::pow(2.3, 3) + std::pow(1.0, 3), 1 / 3.);
+    QTest::newRow("2-norm of null vector") << Victor(0.0, 0.0) << 2 << 0.0;
 }
 
-void TestGeometry::p_normalize()
+void
+TestGeometry::p_norm(void)
 {
     // Data
-    float coord1 = 1;
-    float coord2 = -1;
-    Victor test_victor(coord1, coord2);
-    Victor result_2 = Victor(std::sqrt(2)/2.0, -std::sqrt(2)/2.0);
-    Victor result_3 = Victor(1/std::pow(2, 1/3.0), -1/std::pow(2, 1/3.0));
-    
-    Victor normed_2 = test_victor;
-    normed_2.p_normalize();
-    
-    Victor normed_3 = test_victor;
-    normed_3.p_normalize(3);
-    
+    QFETCH(Victor, test_victor);
+    QFETCH(int, p);
+    QFETCH(float, result);
+
     // Test
-    QCOMPARE(normed_2[0], result_2[0]);
-    QCOMPARE(normed_2[1], result_2[1]);
-    QCOMPARE(normed_3[0], result_3[0]);
-    QCOMPARE(normed_3[1], result_3[1]);
-    
-    
+    QCOMPARE(test_victor.p_norm(p), result);
 }
 
-void TestGeometry::zero()
+void
+TestGeometry::p_normalize_data()
+{
+    QTest::addColumn<Victor>("test_victor");
+    QTest::addColumn<int>("p");
+    QTest::addColumn<Victor>("result");
+
+    QTest::newRow("2-normalize a vector")
+        << Victor(1, -1.0) << 2
+        << Victor(std::sqrt(2) / 2.0, -std::sqrt(2) / 2.0);
+    QTest::newRow("3-normalize a vector")
+        << Victor(1, -1.0) << 3
+        << Victor(1 / std::pow(2, 1 / 3.0), -1 / std::pow(2, 1 / 3.0));
+    QTest::newRow("2-normalize the null vector leaves 0")
+        << Victor(0.0, 0.0) << 2 << Victor(0.0, 0.0);
+}
+
+void
+TestGeometry::p_normalize(void)
+{
+    // Data
+    QFETCH(Victor, test_victor);
+    QFETCH(int, p);
+    QFETCH(Victor, result);
+
+    Victor normed = test_victor;
+    normed.p_normalize(p);
+
+    // Test
+    QCOMPARE(normed[0], result[0]);
+    QCOMPARE(normed[1], result[1]);
+}
+
+void
+TestGeometry::zero()
 {
     // Data
     float coord1 = 1;
     float coord2 = 3;
     Victor test_victor(coord1, coord2);
-    
+
     test_victor.zero();
-    
+
     // Test - Fuzzy compare has issues with comparing to 0 so there's an offset
     QCOMPARE(test_victor[0] + 125.34, 125.34);
     QCOMPARE(test_victor[1] + 125.34, 125.34);
 }
-
-
 
 QTEST_MAIN(TestGeometry)
 #include "test_geometry.moc"
